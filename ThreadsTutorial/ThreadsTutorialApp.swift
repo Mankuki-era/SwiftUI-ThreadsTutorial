e8b871d1d0480b1557dfad4c4bbaec4032b6c286
sasaki <sasaking0215@outlook.jp>
2023-08-21 19:21:49 +0900

    Initial Commit

---
+++ ThreadsTutorial/ThreadsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialApp.swift
+//  ThreadsTutorial
+//
+//  Created by Mankuki_era on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
